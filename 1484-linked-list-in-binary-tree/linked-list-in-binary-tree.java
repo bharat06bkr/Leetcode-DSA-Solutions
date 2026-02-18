2d6a118e6cd86f935b8ec4fd9ea13fc211af19e6
Bhuma Bharat Kumar Reddy <151626959+bharat06bkr@users.noreply.github.com>
2026-02-18 07:06:44 +0530

    Time: 1 ms (98.45%) | Memory: 46.9 MB (63.38%) - LeetSync

---
+++ 1484-linked-list-in-binary-tree/linked-list-in-binary-tree.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution 
+{
+    public boolean isSubPath(ListNode head, TreeNode root) 
+    {
+        if(head==null)  return true;
+        if(root==null)  return false;
+        if(check(head,root))    return true;
+        return isSubPath(head,root.left) || isSubPath(head,root.right);        
+    }
+    public boolean check(ListNode curr,TreeNode root)
+    {
+        // If head reaches end it means there is a path
+        if(curr==null)  return true;
+        // If root reaches last still head not reaches it means there is no path
+        if(root==null )  return false;
+        if(curr.val!=root.val)  return false;
+        return check(curr.next,root.left) || check(curr.next,root.right);
+    }
+}
