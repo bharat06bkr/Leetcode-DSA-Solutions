835a9c2cdd17e11efa354e5e5a1d67edce627a84
Bhuma Bharat Kumar Reddy <151626959+bharat06bkr@users.noreply.github.com>
2026-02-22 07:46:23 +0530

    Time: 1 ms (99.86%) | Memory: 45.2 MB (72.68%) - LeetSync

--- 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.java
+++ 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.java
@@ -2,16 +2,23 @@ class Solution
 {
     public int findLucky(int[] arr) 
     {
-        if(arr[0]==500)  return 500;
-        Map<Integer,Integer> map=new HashMap<>();
-        for(int i=0;i<arr.length;i++)
-            map.put(arr[i],map.getOrDefault(arr[i],0)+1);
-        int max=-1;
-        for(Map.Entry<Integer,Integer> e:map.entrySet())
-        {
-            if(e.getValue()==e.getKey())
-                max=Math.max(max,e.getKey());
-        }  
-        return max;  
+        // if(arr[0]==500)  return 500;
+        // Map<Integer,Integer> map=new HashMap<>();
+        // for(int i=0;i<arr.length;i++)
+        //     map.put(arr[i],map.getOrDefault(arr[i],0)+1);
+        // int max=-1;
+        // for(Map.Entry<Integer,Integer> e:map.entrySet())
+        // {
+        //     if(e.getValue()==e.getKey())
+        //         max=Math.max(max,e.getKey());
+        // }  
+        // return max;  
+        int ans=-1;
+        int freq[]=new int[501];
+        for(int num:arr)
+            freq[num]++;
+        for(int i=1;i<=500;i++)
+            if(freq[i]==i)  ans=i;
+        return ans;
     }
 }
