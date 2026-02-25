1a894f9ce3c0792e3cc1cabd581b1e5915bdd2ae
Bhuma Bharat Kumar Reddy <151626959+bharat06bkr@users.noreply.github.com>
2026-02-25 06:03:05 +0530

    Time: 8 ms (61.40%) | Memory: 46.4 MB (92.84%) - LeetSync

--- 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.java
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.java
@@ -3,18 +3,31 @@ class Solution
 {
     public int[] sortByBits(int[] arr) 
     {
-        List<Map.Entry<Integer,Integer>> list = new ArrayList<>();
-        for(int num : arr)
-            list.add(new AbstractMap.SimpleEntry<>(num, Integer.bitCount(num)));
-        Collections.sort(list,(a,b) ->
+        // List<Map.Entry<Integer,Integer>> list = new ArrayList<>();
+        // for(int num : arr)
+        //     list.add(new AbstractMap.SimpleEntry<>(num, Integer.bitCount(num)));
+        // Collections.sort(list,(a,b) ->
+        // {
+        //     if(!a.getValue().equals(b.getValue()))
+        //         return a.getValue()-b.getValue();
+        //     return a.getKey()-b.getKey();
+        // });
+        // int i=0;
+        // for(Map.Entry<Integer,Integer> l:list)
+        //     arr[i++]=l.getKey();
+        // return arr;
+
+
+        Integer temp[]=new Integer[arr.length];
+        for(int i=0;i<arr.length;i++)   temp[i]=arr[i];
+        Arrays.sort(temp,(a,b) ->
         {
-            if(!a.getValue().equals(b.getValue()))
-                return a.getValue()-b.getValue();
-            return a.getKey()-b.getKey();
+            int v1=Integer.bitCount(a);
+            int v2=Integer.bitCount(b);
+            if(v1==v2)  return a-b;
+            return v1-v2;
         });
-        int i=0;
-        for(Map.Entry<Integer,Integer> l:list)
-            arr[i++]=l.getKey();
+        for(int i=0;i<arr.length;i++)   arr[i]=temp[i];
         return arr;
     }
 }
