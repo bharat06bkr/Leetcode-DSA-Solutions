8d638b48d5a0ef53ce3e593f9837bf6792b006d1
Bhuma Bharat Kumar Reddy <151626959+bharat06bkr@users.noreply.github.com>
2026-03-05 17:44:40 +0530

    Time: 50 ms (22.75%) | Memory: 47.1 MB (89.52%) - LeetSync

--- 1561-rearrange-words-in-a-sentence/rearrange-words-in-a-sentence.java
+++ 1561-rearrange-words-in-a-sentence/rearrange-words-in-a-sentence.java
@@ -9,9 +9,13 @@ class Solution
         //         return a.length()-b.length();
         //     return a.length();
         // });
-        List<String> list = new ArrayList<>(Arrays.asList(arr));
-        list.sort((a, b) -> Integer.compare(a.length(), b.length()));
-        String s=String.join(" ",list); 
+        // List<String> list = new ArrayList<>(Arrays.asList(arr));
+        // list.sort((a, b) -> Integer.compare(a.length(), b.length()));
+        Arrays.sort(arr,(a,b)->
+        {
+            return a.length()-b.length();
+        });
+        String s=String.join(" ",arr); 
         s=s.substring(0,1).toUpperCase()+s.substring(1).toLowerCase();
         return s;   
     }
