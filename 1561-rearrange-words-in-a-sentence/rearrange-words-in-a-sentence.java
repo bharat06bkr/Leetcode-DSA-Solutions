431b60b4fe9ce2e0e0657c745a79aed2a958e142
Bhuma Bharat Kumar Reddy <151626959+bharat06bkr@users.noreply.github.com>
2026-03-05 17:43:14 +0530

    Time: 53 ms (21.86%) | Memory: 47.3 MB (83.23%) - LeetSync

---
+++ 1561-rearrange-words-in-a-sentence/rearrange-words-in-a-sentence.java
@@ -0,0 +1,18 @@
+class Solution 
+{
+    public String arrangeWords(String text) 
+    {
+        String arr[]=text.split("\\s+");
+        // Arrays.sort(arr,(a,b)->
+        // {
+        //     if(a.length()!=b.length())    // No stable sort
+        //         return a.length()-b.length();
+        //     return a.length();
+        // });
+        List<String> list = new ArrayList<>(Arrays.asList(arr));
+        list.sort((a, b) -> Integer.compare(a.length(), b.length()));
+        String s=String.join(" ",list); 
+        s=s.substring(0,1).toUpperCase()+s.substring(1).toLowerCase();
+        return s;   
+    }
+}
