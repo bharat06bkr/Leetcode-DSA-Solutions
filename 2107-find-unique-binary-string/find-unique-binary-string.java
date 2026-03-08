9fb2444b8aa0256781d576e68bb814b4d8959f5c
Bhuma Bharat Kumar Reddy <151626959+bharat06bkr@users.noreply.github.com>
2026-03-08 08:31:00 +0530

    Time: 1 ms (59.94%) | Memory: 42.9 MB (97.12%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.java
+++ 2107-find-unique-binary-string/find-unique-binary-string.java
@@ -1,27 +1,34 @@
 class Solution 
 {
-    int length;
-    public String findDifferentBinaryString(String[] nums) 
+    // int length;
+    // public String findDifferentBinaryString(String[] nums) 
+    // {
+    //     length=nums[0].length();
+    //     List<String> l=new ArrayList<>();        
+    //     List<String> list=new ArrayList<>(Arrays.asList(nums));
+    //     generateBinaryString("",l);
+    //     for(String str:l)
+    //         if(!list.contains(str))   return str;
+    //     char arr[]=new char[length];
+    //     Arrays.fill(arr,'0');    passes but most optimal
+    //     return new String(arr);
+    // }
+    // public void generateBinaryString(String s,List<String> l)
+    // {
+    //     if(s.length()==length)  
+    //     {
+    //         l.add(s);
+    //         return;
+    //     }
+    //     generateBinaryString(s+"1",l);
+    //     if(s.isEmpty() || s.charAt(s.length()-1)!='0')
+    //         generateBinaryString(s+"0",l);
+    // }
+    public String findDifferentBinaryString(String[] nums)
     {
-        length=nums[0].length();
-        List<String> l=new ArrayList<>();        
-        List<String> list=new ArrayList<>(Arrays.asList(nums));
-        generateBinaryString("",l);
-        for(String str:l)
-            if(!list.contains(str))   return str;
-        char arr[]=new char[length];
-        Arrays.fill(arr,'0');
-        return new String(arr);
-    }
-    public void generateBinaryString(String s,List<String> l)
-    {
-        if(s.length()==length)  
-        {
-            l.add(s);
-            return;
-        }
-        generateBinaryString(s+"1",l);
-        if(s.isEmpty() || s.charAt(s.length()-1)!='0')
-            generateBinaryString(s+"0",l);
+       StringBuilder sb=new StringBuilder();
+       for(int i=0;i<nums.length;i++)
+            sb.append(nums[i].charAt(i)=='0' ? '1':'0');
+        return sb.toString();
     }
 }
