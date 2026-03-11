abe902d9d53e5aed79d90ea1c97f3d3ee5cedcd6
Bhuma Bharat Kumar Reddy <151626959+bharat06bkr@users.noreply.github.com>
2026-03-11 05:57:46 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (73.40%) - LeetSync

--- 476-number-complement/number-complement.java
+++ 476-number-complement/number-complement.java
@@ -1,8 +1,12 @@
 class Solution {
     public int findComplement(int n) {
-        String s="";
-        for(char c:Integer.toString(n,2).toCharArray())
-            s=s+((c=='1')?"0":"1");
-        return Integer.parseInt(s,2); 
+        // String s="";
+        // for(char c:Integer.toString(n,2).toCharArray())
+        //     s=s+((c=='1')?"0":"1");
+        // return Integer.parseInt(s,2); 
+        int bit=1;
+        while(bit<n)
+            bit=(bit<<1)|1;
+        return bit^n;
     }
 }
