d1d01b085ab31b4efd27058633f5ab7fc9fa9000
Bhuma Bharat Kumar Reddy <151626959+bharat06bkr@users.noreply.github.com>
2026-08-01 17:03:23 +0530

    Time: 1 ms (14.32%) | Memory: 41.9 MB (83.35%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,23 +1,37 @@
 class Solution 
 {
     public int uniquePaths(int m, int n) 
+   {
+        int dp[][]=new int[m][n];
+        // for(int[] d:dp)
+        //     Arrays.fill(d,-1);
+       // return meth(m-1,n-1,dp);
+       for(int i=0;i<m;i++)
+       {
+           
+           for(int j=0;j<n;j++)
+           {
+               if(i==0 && j==0) dp[0][0]=1;
+               else
+               {
+                   int left=0;
+                   int right=0;
+                   if(i>0)  left=dp[i-1][j];
+                   if(j>0) right=dp[i][j-1];
+                   dp[i][j]=left+right;
+               }
+           }
+       }
+       return dp[m-1][n-1];
+    }
+    public int meth(int i,int j,int dp[][])
     {
-        int [][] dp=new int[m][n];
-
-    for (int  j=0;j<n;j++){
-      dp[0][j]=1;
-    }
-
-    for (int i=0;i<m;i++){
-      dp[i][0]=1;
-    }
-    for (int i=1;i<m;i++){
-      for (int j=1;j<n;j++){
-        dp[i][j]=dp[i-1][j]+dp[i][j-1];
-        
-      }
-      
-    }
-    return dp[m-1][n-1];
+        if(i==0 && j==0) return 1;
+        if(i<0 || j<0)  return 0;
+        if(dp[i][j]!=-1)    return dp[i][j];
+        int left=meth(i,j-1,dp);
+        int right=meth(i-1,j,dp);
+        dp[i][j]=left+right;
+        return left+right;
     }
 }
